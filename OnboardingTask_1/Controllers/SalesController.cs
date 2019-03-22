4e14c381cf1818a034f2c757bf1879667eac8219
venkat gadicherla <venkat_gadicherla@hotmail.com>
2019-03-22 14:19:09 +1100

    Add project files.

---
+++ OnboardingTask_1/Controllers/SalesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OnboardingTask_1.Models;
+
+namespace OnboardingTask_1.Controllers
+{
+    public class SalesController : Controller
+    {
+        private CustomerDbEntities db = new CustomerDbEntities();
+
+        // GET: Sales
+        public ActionResult Index()
+        {
+            var productSolds = db.ProductSolds.Include(p => p.Customer).Include(p => p.Product).Include(p => p.Store);
+            return View(productSolds.ToList());
+        }
+
+        // GET: Sales/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductSold productSold = db.ProductSolds.Find(id);
+            if (productSold == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productSold);
+        }
+
+        // GET: Sales/Create
+        public ActionResult Create()
+        {
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Name");
+            ViewBag.ProductId = new SelectList(db.Products, "Id", "Name");
+            ViewBag.StoreId = new SelectList(db.Stores, "Id", "Name");
+            return View();
+        }
+
+        // POST: Sales/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,ProductId,CustomerId,StoreId,Date")] ProductSold productSold)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProductSolds.Add(productSold);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Name", productSold.CustomerId);
+            ViewBag.ProductId = new SelectList(db.Products, "Id", "Name", productSold.ProductId);
+            ViewBag.StoreId = new SelectList(db.Stores, "Id", "Name", productSold.StoreId);
+            return View(productSold);
+        }
+
+        // GET: Sales/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductSold productSold = db.ProductSolds.Find(id);
+            if (productSold == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Name", productSold.CustomerId);
+            ViewBag.ProductId = new SelectList(db.Products, "Id", "Name", productSold.ProductId);
+            ViewBag.StoreId = new SelectList(db.Stores, "Id", "Name", productSold.StoreId);
+            return View(productSold);
+        }
+
+        // POST: Sales/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,ProductId,CustomerId,StoreId,Date")] ProductSold productSold)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productSold).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Name", productSold.CustomerId);
+            ViewBag.ProductId = new SelectList(db.Products, "Id", "Name", productSold.ProductId);
+            ViewBag.StoreId = new SelectList(db.Stores, "Id", "Name", productSold.StoreId);
+            return View(productSold);
+        }
+
+        // GET: Sales/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductSold productSold = db.ProductSolds.Find(id);
+            if (productSold == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productSold);
+        }
+
+        // POST: Sales/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProductSold productSold = db.ProductSolds.Find(id);
+            db.ProductSolds.Remove(productSold);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ OnboardingTask_1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnboardingTask_1.MvcApplication" Language="C#" %>
